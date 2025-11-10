7935e72a10b09d20e08a7f1c2163ee48a0f43eac
Beanofbean <jubarrioscastro@s.sfusd.edu>
2025-11-10 01:49:48 -0800

    done for moddlue 7

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -1,33 +1,79 @@
 extends Control
 
-var dialogue_items: Array[String] = [
-	"creegan is the goat",
-	"Почему школа такая длинная?",
-	"yo quiero ir a la casa",
-	"この授業はいつ終わるのですか",
+var bodies := {
+	"sophia": preload("res://assets/sophia.png"),
+	"pink": preload("res://assets/pink.png")
+}
+
+var expressions := {
+	"happy": preload ("res://assets/emotion_happy.png"),
+	"regular": preload ("res://assets/emotion_regular.png"),
+	"sad": preload ("res://assets/emotion_sad.png"),
+}
+
+var dialogue_items: Array[Dictionary] = [
+	{
+		"expression": expressions["regular"],
+		"text": "Creegan is the [shake]goat[/shake]!",
+		"character": bodies["sophia"],
+	},
+	{
+		"expression": expressions["sad"],
+		"text":  "Почему школа такая длинная?",
+		"character": bodies["pink"],
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "yo quiero ir a la [tornado freq=3.0][rainbow val=1.0]casa[/rainbow][/tornado]",
+		"character": bodies["sophia"],
+	},
+	{
+		"expression": expressions["sad"],
+		"text": "この授業はいつ終わるのですか",
+		"character": bodies["pink"],
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "I get to leave [wave]finally[/wave].",
+		"character": bodies["sophia"],
+	},
 ]
 var current_item_index := 0
 
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var next_button: Button = %NextButton
 @onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
+@onready var body: TextureRect = %Body
+@onready var expression: TextureRect = %Expression
 
 
 func _ready() -> void:
 	show_text()
 	next_button.pressed.connect(advance)
 
+
 func show_text() -> void:
 	var current_item := dialogue_items[current_item_index]
-	rich_text_label.text = current_item
+	rich_text_label.text = current_item["text"]
+	expression.texture = current_item["expression"]
+	body.texture = current_item["character"]
 	rich_text_label.visible_ratio = 0.0
 	var tween := create_tween()
-	var text_appearing_duration := 1.2
+	var text_appearing_duration: float = current_item["text"].length() / 30.0
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
+
 	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
 	var sound_start_position := randf() * sound_max_offset
 	audio_stream_player.play(sound_start_position)
 	tween.finished.connect(audio_stream_player.stop)
+
+	slide_in()
+	
+	next_button.disabled = true
+	tween.finished.connect(func() -> void:
+		next_button.disabled = false
+	)
+
 
 func advance() -> void:
 	current_item_index += 1
@@ -35,3 +81,13 @@ func advance() -> void:
 		get_tree().quit()
 	else:
 		show_text()
+
+
+func slide_in() -> void:
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_QUART)
+	tween.set_ease(Tween.EASE_OUT)
+	body.position.x = 500.0
+	tween.tween_property(body, "position:x", 0.0, 0.5)
+	body.modulate.a = 0.0
+	tween.parallel().tween_property(body, "modulate:a", 1.0, 0.2)
